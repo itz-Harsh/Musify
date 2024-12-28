8abff0482199ac97f7f1a8aa3ed6dba617538ba0
Harsh Suthar <hsuthar1125@gmail.com>
2024-12-28 23:19:38 +0530

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 // main.jsx or index.jsx
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 import App from "./App"; // Ensure the path is correct
 import "./index.css"; // Add styles if required
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>
+
 );
