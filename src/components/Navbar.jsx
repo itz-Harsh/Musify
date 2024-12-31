805ee7bda41d7312617d075505d200f5825d1630
Harsh Suthar <hsuthar1125@gmail.com>
2024-12-31 18:37:34 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         <div>
           <form
             onSubmit={handleSearch}
-            className="flex items-center w-full m-3"
+            className="hidden lg:flex items-center w-full m-3"
           >
             <input
               type="text"
