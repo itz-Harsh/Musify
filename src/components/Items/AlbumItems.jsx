31d470a09d76a0c83db757bd9c751eae0fb1c320
Harsh Suthar <hsuthar1125@gmail.com>
2025-02-02 22:47:34 +0530

    Complete

--- src/components/Items/AlbumItems.jsx
+++ src/components/Items/AlbumItems.jsx
@@ -3,8 +3,8 @@ import he from "he";
 
 const AlbumItems = ({ name, artists, id, image }) => {
   // Ensure 'artists' is an array and fallback if empty or undefined
-  const artistNames = Array.isArray(artists?.primary) 
-    ? artists.primary.map((artist) => artist.name).join(" , ") 
+  const artistNames = Array.isArray(artists?.primary)
+    ? artists.primary.map((artist) => artist.name).join(" , ")
     : "";
 
   // Ensure image is an array with at least 3 elements, or provide a fallback image
@@ -15,23 +15,23 @@ const AlbumItems = ({ name, artists, id, image }) => {
       to={`/albums/${id}`}
       className="card  w-[9.5rem] h-[11.96rem] overflow-clip  border-[0.1px]  p-1  rounded-lg"
     >
-      <div className="p-1" >
-      <img
-        src={imageUrl  || "/Unknown.png" }
-        alt={name}
-        className="rounded-lg imgs"
-      />
-      
+      <div className="p-1">
+        <img
+          src={imageUrl || "/Unknown.png"}
+          alt={name}
+          className="rounded-lg imgs"
+        />
       </div>
       <div className="text-[13px] w-full flex flex-col justify-center pl-2">
-        <span className="font-semibold overflow-x-clip">{name
-                                    ? he.decode(name)
-                                    : "Empty"}</span>
-        <span className="flex gap-1">by<p className="font-semibold">{artistNames}</p></span>
+        <span className="font-semibold overflow-x-clip">
+          {name ? he.decode(name) : "Empty"}
+        </span>
+        <span className="flex gap-1">
+          by<p className="font-semibold">{artistNames}</p>
+        </span>
       </div>
     </Link>
   );
 };
 
 export default AlbumItems;
-
---
+++ src/genreData.jsx
@@ -0,0 +1,401 @@
+
+
+export const genreData = {
+    "For You": [
+    {
+        "id": "1134543272",
+        "name": "Hindi: India Superhits Top 50",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/Hindi-IndiaSuperhitsTop50_20250131013815_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/Hindi-IndiaSuperhitsTop50_20250131013815_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/Hindi-IndiaSuperhitsTop50_20250131013815_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/hindi-india-superhits-top-50/zlJfJYVuyjpxWb5,FqsjKg__",
+        "songCount": 50,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "1134595537",
+        "name": "English: India Superhits Top 50",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/English-IndiaSuperhitsTop50_20250124035621_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/English-IndiaSuperhitsTop50_20250124035621_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/English-IndiaSuperhitsTop50_20250124035621_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/english-india-superhits-top-50/aXoCADwITrUCObrEMJSxEw__",
+        "songCount": 50,
+        "language": "english",
+        "explicitContent": false
+      },
+      {
+        "id": "110858205",
+        "name": "Trending Today",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/charts_TrendingToday_134351_20230826113717_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/charts_TrendingToday_134351_20230826113717_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/charts_TrendingToday_134351_20230826113717_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/trending-today/I3kvhipIy73uCJW60TJk1Q__",
+        "songCount": 597,
+        "language": "english",
+        "explicitContent": false
+      },
+      {
+        "id": "946682072",
+        "name": "Most Searched Songs - Hindi",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/MostSearchedSongsHindi_20241223050747_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/MostSearchedSongsHindi_20241223050747_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/MostSearchedSongsHindi_20241223050747_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/most-searched-songs-hindi/csv-SfcHUmHc1EngHtQQ2g__",
+        "songCount": 30,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "945969391",
+        "name": "Most Searched Songs - English",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/MostSearchedSongsEnglish_20241029063059_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/MostSearchedSongsEnglish_20241029063059_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/MostSearchedSongsEnglish_20241029063059_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/most-searched-songs-english/xUOBWZUG6AgGSw2I1RxdhQ__",
+        "songCount": 30,
+        "language": "english",
+        "explicitContent": false
+      },
+      {
+        "id": "85481065",
+        "name": "Fresh Tunes",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/FreshTunes_20250131112713_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/FreshTunes_20250131112713_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/FreshTunes_20250131112713_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/fresh-tunes/Ns2UZo9qDvI_",
+        "songCount": 50,
+        "language": "english",
+        "explicitContent": false
+      },
+      {
+        "id": "1191141029",
+        "name": "Best Of Romance - Arijit Singh",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/BestofRomanceArijitSingh_20231005095622_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/BestofRomanceArijitSingh_20231005095622_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/BestofRomanceArijitSingh_20231005095622_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/best-of-romance-arijit-singh/SaQiNubxPJil7JpGxdAPvw__",
+        "songCount": 57,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "903166403",
+        "name": "Best Of Romance - Hindi",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/BestOfRomanceHindi_20250131090151_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/BestOfRomanceHindi_20250131090151_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/BestOfRomanceHindi_20250131090151_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/best-of-romance-hindi/SBKnUgjNeMIwkg5tVhI3fw__",
+        "songCount": 50,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "84576200",
+        "name": "Groovy Love Songs - English",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/GroovyLoveSongsEnglish_20241014111735_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/GroovyLoveSongsEnglish_20241014111735_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/GroovyLoveSongsEnglish_20241014111735_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/groovy-love-songs-english/bMqWZ8RYo1M_",
+        "songCount": 50,
+        "language": "english",
+        "explicitContent": false
+      },
+      {
+        "id": "932189657",
+        "name": "Best Of Dance - Hindi",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/BestOfDanceHindi_20241121062228_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/BestOfDanceHindi_20241121062228_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/BestOfDanceHindi_20241121062228_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/best-of-dance-hindi/qVvfieICUY5ieSJqt9HmOQ__",
+        "songCount": 50,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "158221835",
+        "name": "Best Of Sad Songs - Hindi",
+        "type": "playlist",
+        "image": [
+            {
+              "quality": "50x50",
+              "url": "https://c.saavncdn.com/editorial/BestOfSadSongsHindi_20240627124807.jpg?bch=1738056560"
+            },
+            {
+              "quality": "150x150",
+              "url": "https://c.saavncdn.com/editorial/BestOfSadSongsHindi_20240627124807.jpg?bch=1738056560"
+            },
+            {
+              "quality": "500x500",
+              "url": "https://c.saavncdn.com/editorial/BestOfSadSongsHindi_20240627124807.jpg?bch=1738056560"
+            }
+          ],
+        "url": "https://www.jiosaavn.com/featured/best-of-sad-songs-hindi/WjjhR,A3iLLuCJW60TJk1Q__",
+        "songCount": 10,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "101704478",
+        "name": "Jhakaas Remakes",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/JhakaasRemakes_20240909162434_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/JhakaasRemakes_20240909162434_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/JhakaasRemakes_20240909162434_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/jhakaas-remakes/7e2LtwVBX6JFo9wdEAzFBA__",
+        "songCount": 25,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "156473621",
+        "name": "Asli Pop",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/AsliPop_20250108043114_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/AsliPop_20250108043114_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/AsliPop_20250108043114_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/asli-pop/pDQtHvJRh4IGSw2I1RxdhQ__",
+        "songCount": 25,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "156710699",
+        "name": "Bollywood Rock - Workout Mix",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/BollywoodRockWorkoutMix_20240229050234_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/BollywoodRockWorkoutMix_20240229050234_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/BollywoodRockWorkoutMix_20240229050234_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/bollywood-rock-workout-mix/pkqEoKN5hGqO0eMLZZxqsA__",
+        "songCount": 27,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "1200360637",
+        "name": "Bollywood Workout Hits",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/BollywoodWorkoutHits_20230925070507_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/BollywoodWorkoutHits_20230925070507_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/BollywoodWorkoutHits_20230925070507_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/bollywood-workout-hits/fABpRRfmRkQCObrEMJSxEw__",
+        "songCount": 51,
+        "language": "hindi",
+        "explicitContent": false
+      },
+      {
+        "id": "1134543511",
+        "name": "Punjabi: India Superhits Top 50",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/Punjabi-IndiaSuperhitsTop50_20250124050242_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/Punjabi-IndiaSuperhitsTop50_20250124050242_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/Punjabi-IndiaSuperhitsTop50_20250124050242_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/punjabi-india-superhits-top-50/wSwarbl2bQSrB59Sr2unUQ__",
+        "songCount": 50,
+        "language": "punjabi",
+        "explicitContent": false
+      },
+      {
+        "id": "4876538",
+        "name": "Punjabi Workout",
+        "type": "playlist",
+        "image": [
+          {
+            "quality": "50x50",
+            "url": "https://c.saavncdn.com/editorial/PunjabiWorkout_20240702045755_50x50.jpg"
+          },
+          {
+            "quality": "150x150",
+            "url": "https://c.saavncdn.com/editorial/PunjabiWorkout_20240702045755_150x150.jpg"
+          },
+          {
+            "quality": "500x500",
+            "url": "https://c.saavncdn.com/editorial/PunjabiWorkout_20240702045755_500x500.jpg"
+          }
+        ],
+        "url": "https://www.jiosaavn.com/featured/punjabi-workout/1lv8piStXj4_",
+        "songCount": 50,
+        "language": "punjabi",
+        "explicitContent": false
+      },
+
+    ]
+};
+    
+  
+
